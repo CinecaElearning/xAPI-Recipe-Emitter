8a3927e0e8c538e9f0f8a33318772f9d1b03ab1c
Supporto Cineca <elearning-supp@cineca.it>
2016-03-14 14:34:44 +0100

    functioning with course completed event on all 3 levels

--- src/Events/CourseCompleted.php
+++ src/Events/CourseCompleted.php
@@ -1,6 +1,10 @@
 <?php namespace XREmitter\Events;
 
 class CourseCompleted extends Event {
+    protected static $verb_display = [
+        'en' => 'completed'
+    ];
+
     /**
      * Reads data for an event.
      * @param [String => Mixed] $opts
@@ -10,6 +14,10 @@ class CourseCompleted extends Event {
     public function read(array $opts) {
         return array_merge(parent::read($opts), [
             'object' => $this->readCourse($opts),
+            'verb' => [
+                'id' => 'http://adlnet.gov/expapi/verbs/completed',
+                'display' => $this->readVerbDisplay($opts),
+            ],
         ]);
     }
 }
