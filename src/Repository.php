d6af3f8fb27f19a422a3cabb9daf86d96b07c27c
Supporto Cineca <elearning-supp@cineca.it>
2016-03-24 18:24:44 +0100

    Repository cleaned

--- src/Repository.php
+++ src/Repository.php
@@ -21,15 +21,9 @@ class Repository extends PhpObj {
      * @return [string => mixed]
      */
     public function createEvents(array $statements) {
-        error_log("emitter repository statements ". var_export($statements, true) ."\r\n", 3, '/var/www/html/admin/tool/log/store/xapi/classes/log/error_log.txt');
-
         $LRSResponse = $this->store->saveStatements($statements);
-
-        error_log("emitter repository LRSResponse ". var_export($LRSResponse, true) ."\r\n", 3, '/var/www/html/admin/tool/log/store/xapi/classes/log/error_log.txt');
-
-               if (isset($LRSResponse->success) && $LRSResponse->success) $statements['last_action_result'] = 'success';
-               else $statements['last_action_result'] = 'error';
-
+		if (isset($LRSResponse->success) && $LRSResponse->success) $statements['last_action_result'] = 'success';
+		else $statements['last_action_result'] = 'error';
         return $statements;
     }
 }
